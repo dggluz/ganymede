599c97ee29564d113226fc1a80d9bd4bd5eb7af0
Gonza Gluzman <dggluz@gmail.com>
2018-08-30 12:31:38 -0300

    chore(pingCtrl): jsdocs

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,6 +1,9 @@
 import { Task } from '@ts-task/task';
 import { createEndpoint } from '../server-utils/create-endpoint';
 
+/**
+ * Endpoint that just response with a dummy object. Useful to check if the server is alive.
+ */
 export const pingCtrl = createEndpoint(_ =>
 	Task
 		.resolve({
